5efaa2258cb7a3b01e36764cecef0a77265ee878
miasilvia <135966912+miasilvia@users.noreply.github.com>
2023-06-20 18:34:28 +0700

    tombol reset

--- js/hitung.js
+++ js/hitung.js
@@ -1,6 +1,12 @@
-function hitungKonversi() {
-    var Fehrentheit = document.getElementById("Fehrentheit").value;
-    var konversi = (1.8 * Fehrentheit) + 32;
+function hitungKonversi() {  //ini adalah fungsi dengan nama hitungKonversi
+    var Fehrentheit = document.getElementById("Fehrentheit").value; //ini variabel Ferentheit
+    var konversi = (1.8 * Fehrentheit) + 32;  //ini variabel konversi untuk menentukan rumus perhitungan
     document.getElementById("konversi").innerHTML = konversi;
     document.getElementById("Fehrentheit2").innerHTML = Fehrentheit + ("&#176 * (9/5) + 32 = ") + konversi + ("&#176F");
+    
+  }
+  function resetFields() { //fungsi untuk reset form
+    document.getElementById("Fehrentheit").value = "";
+    document.getElementById("konversi").value = "";
+    document.getElementById("Fehrentheit2").value = "";
   }
