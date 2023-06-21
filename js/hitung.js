be9ddd9aec517941cfc7a2f38b7413b88b737072
miasilvia <135966912+miasilvia@users.noreply.github.com>
2023-06-21 12:55:45 +0700

    rev5

--- js/hitung.js
+++ js/hitung.js
@@ -3,7 +3,7 @@ function hitungFehrentheit() {  //ini adalah fungsi dengan nama hitungKonversi
     var konversi = (1.8 * Celcius) + 32;  //ini variabel konversi untuk menentukan rumus perhitungan
     document.getElementById("Fehrentheit").innerHTML = konversi;
     document.getElementById("kalkulasi").innerHTML = Celcius + ("&#176 * (9/5) + 32 = ") + konversi + ("&#176F");
-    
+   
   }
   function resetFields() { //fungsi untuk reset form
     document.getElementById("Celcius").value = "";
@@ -14,5 +14,5 @@ function hitungFehrentheit() {  //ini adalah fungsi dengan nama hitungKonversi
     var Fehrentheit = document.getElementById("Fehrentheit").value;
     var konversi2 = (5/9) * (Fehrentheit - 32);
     document.getElementById("Celcius").innerHTML = konversi2;
-    document.getElementById("kalkulasi").innerHTML = ("5/9 * ") + (Fehrentheit + "&#176 - 32");
+    document.getElementById("kalkulasi").innerHTML = ("5/9 * ") + (Fehrentheit + "&#176 - 32 = " + konversi2);
   }
