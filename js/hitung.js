da9aa504d4145e08c27bb18a8f87d67347d4700a
miasilvia <135966912+miasilvia@users.noreply.github.com>
2023-06-21 17:31:30 +0700

    end fix-2

--- js/hitung.js
+++ js/hitung.js
@@ -3,7 +3,7 @@ function hitungFehrentheit() {  //ini adalah fungsi dengan nama hitungKonversi
   var konversi = (1.8 * Celcius) + 32;  //ini variabel konversi untuk menentukan rumus perhitungan
   document.getElementById("Fehrentheit").innerHTML = konversi;
   document.getElementById("kalkulasi").innerHTML = Celcius + ("&#176 * (9/5) + 32 = ") + konversi + ("&#176F");
-  document.getElementById("rumus").innerHTML = "Konversi Suhu dari Celcius (&#176C) ke Fahrenheit (F) <br> F = (9/5) C + 32 <br> F = suhu dalam skala Fahrenheit <br> C = suhu dalam skala Celcius"
+  document.getElementById("rumus").innerHTML = "Konversi Suhu dari Celcius (&#176C) ke Fahrenheit (&#176F) <br> &#176F = (9/5) &#176C + 32 <br> &#176F = suhu dalam skala Fahrenheit <br> &#176C = suhu dalam skala Celcius"
 }
 function resetFields() { //fungsi untuk reset form
   document.getElementById("Celcius").value = "";
@@ -16,5 +16,5 @@ function hitungCelcius() {
   var konversi2 = (5 / 9) * (Fehrentheit - 32);
   document.getElementById("Celcius").innerHTML = konversi2;
   document.getElementById("kalkulasi").innerHTML = ("5/9 * ") + (Fehrentheit + "&#176 - 32 = " + konversi2);
-  document.getElementById("rumus").innerHTML = "Konversi Suhu dari Fahrenheit (F) ke Celcius (&#176C) <br> C = (5/9) *(F&#176 + 32) <br> F = suhu dalam skala Fahrenheit <br> C = suhu dalam skala Celcius"
+  document.getElementById("rumus").innerHTML = "Konversi Suhu dari Fahrenheit (&#176F) ke Celcius (&#176C) <br> &#176C = (5/9) *(F&#176 + 32) <br> &#176F = suhu dalam skala Fahrenheit <br> &#176C = suhu dalam skala Celcius"
 }
